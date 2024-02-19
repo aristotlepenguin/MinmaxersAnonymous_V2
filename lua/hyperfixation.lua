0009067ddbfd232dd047d7ec1724d493d3dc6a55
aristotlepenguin <danielpengra@gmail.com>
2024-02-18 19:07:26 -0600

    hyperfixation works now

--- lua/hyperfixation.lua
+++ lua/hyperfixation.lua
@@ -3,6 +3,15 @@ local sfx = SFXManager()
 local game = Game()
 
 
+function mod:GetPillFromEffect(effect, player)
+    local itempool = game:GetItemPool()
+    for i=0, PillColor.NUM_STANDARD_PILLS-1, 1 do
+        if effect == itempool:GetPillEffect(i, player) then
+            return i
+        end
+    end
+end
+
 function mod:FixationUseCard(card, player, flags)
     if mod.MMA_GlobalSaveData.fixationVariant == nil and player:GetCollectibleNum(mod.MMATypes.COLLECTIBLE_HYPERFIXATION) > 0 then
         mod.MMA_GlobalSaveData.fixationType = card
@@ -15,7 +24,7 @@ mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.FixationUseCard)
 
 function mod:FixationUsePill(pill, player, flags)
     if mod.MMA_GlobalSaveData.fixationVariant == nil and player:GetCollectibleNum(mod.MMATypes.COLLECTIBLE_HYPERFIXATION) > 0 then
-        mod.MMA_GlobalSaveData.fixationType = pill
+        mod.MMA_GlobalSaveData.fixationType = mod:GetPillFromEffect(pill, player)
         mod.MMA_GlobalSaveData.fixationVariant = PickupVariant.PICKUP_PILL
         sfx:Play(SoundEffect.SOUND_GOLDENKEY, Options.SFXVolume*2)
         player:AddNullCostume(mod.MMATypes.COSTUME_HYPERFIXATION)
@@ -35,9 +44,20 @@ function mod:FixationPostPickupInit(pickup, itempool, decrease, seed)
             end
         end)
         if hasIt then
+            print("morphed")
             pickup:ToPickup():Morph(EntityType.ENTITY_PICKUP, mod.MMA_GlobalSaveData.fixationVariant, mod.MMA_GlobalSaveData.fixationType, true)
         end
     end
 end
 mod:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, mod.FixationPostPickupInit)
+--mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, mod.FixationPostPickupInit)
+
+function mod:fixationPillInit(seed)
+    if mod.MMA_GlobalSaveData.fixationVariant == PickupVariant.PICKUP_PILL then
+        return mod.MMA_GlobalSaveData.fixationType
+    else
+        return nil
+    end
+end
+mod:AddCallback(ModCallbacks.MC_GET_PILL_COLOR, mod.fixationPillInit)
 
