864b287645e5b3690052d316eecccf84e0f09843
aristotlepenguin <danielpengra@gmail.com>
2024-05-26 19:20:02 -0500

    fix to the item filter

--- lua/misc.lua
+++ lua/misc.lua
@@ -3,23 +3,26 @@ local game = Game()
 local itemPool = game:GetItemPool()
 local rng  =RNG()
 
-function mod:itemSwitch_MMA(pickup, variant, subtype)
-    if not mod.MenuData or not mod.MenuData.ItemSwitch or (variant ~= PickupVariant.PICKUP_COLLECTIBLE and variant ~= PickupVariant.PICKUP_SHOPITEM) then
+function mod:itemSwitch_MMA(pickup)
+    if not mod.MenuData or not mod.MenuData.ItemSwitch or (pickup.Variant ~= PickupVariant.PICKUP_COLLECTIBLE) then
         return
     end
     
-    if mod.MenuData.ItemSwitch[subtype] == 2 or (mod.MenuData.ItemSwitch[subtype] ~= 2 and subtype == mod.MMATypes.COLLECTIBLE_D_SQRT) then
+    if mod.MenuData.ItemSwitch[pickup.SubType] == 2 or (mod.MenuData.ItemSwitch[pickup.SubType] ~= 2 and pickup.SubType == mod.MMATypes.COLLECTIBLE_D_SQRT) then
         local room = game:GetRoom()
         local new_random_item
         local pool = itemPool:GetPoolForRoom (room:GetType(), rng:GetSeed())
 
         for i=0, 10000, 1 do
-            new_random_item = itemPool:GetCollectible(pool, true, rng:RandomInt(100000)+1)
+            new_random_item = itemPool:GetCollectible(pool, true)
             if mod.MenuData.ItemSwitch[new_random_item] ~= 2 then
                 break
             end
         end
-        return {PickupVariant.PICKUP_COLLECTIBLE, new_random_item}
+        --return {PickupVariant.PICKUP_COLLECTIBLE, new_random_item}
+        pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, new_random_item, true)
     end
 end
-mod:AddCallback(ModCallbacks.MC_POST_PICKUP_SELECTION, mod.itemSwitch_MMA)
+--mod:AddCallback(ModCallbacks.MC_POST_PICKUP_SELECTION, mod.itemSwitch_MMA)
+--MC_POST_PICKUP_UPDATE
+mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, mod.itemSwitch_MMA)
