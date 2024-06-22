da49357ce4a7605712573a7357ecee646ce93989
aristotlepenguin <danielpengra@gmail.com>
2024-06-22 17:06:24 -0500

    fixed blank pedestal rerolling

--- lua/dsqrroot.lua
+++ lua/dsqrroot.lua
@@ -13,13 +13,15 @@ function mod:useDSqrt(collectible, rng, player, useflags, activeslot, customvard
     local totalItems = itemconfig:GetCollectibles().Size-1
     
     for i, entity in ipairs(entities) do
-        Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, -1, entity.Position, entity.Velocity, player)
-        local oldItem = entity.SubType
-        local newItem = (((bombs * oldItem * oldItem) + (coins * oldItem) + secondsOnTimer + 109)/9) % totalItems
-        if newItem % 1 ~= 0 or oldItem == CollectibleType.COLLECTIBLE_POOP then
-            newItem = CollectibleType.COLLECTIBLE_POOP
+        if entity.SubType ~=0 then
+            Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, -1, entity.Position, entity.Velocity, player)
+            local oldItem = entity.SubType
+            local newItem = (((bombs * oldItem * oldItem) + (coins * oldItem) + secondsOnTimer + 109)/9) % totalItems
+            if newItem % 1 ~= 0 or oldItem == CollectibleType.COLLECTIBLE_POOP then
+                newItem = CollectibleType.COLLECTIBLE_POOP
+            end
+            entity:ToPickup():Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, newItem, true)
         end
-        entity:ToPickup():Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, newItem, true)
     end
     return {
         Discharge = true,
